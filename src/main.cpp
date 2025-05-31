73e0d1ab21ac67f61d19004310b4602f7118b04f
dsantiago8 <93739360+dsantiago8@users.noreply.github.com>
2025-05-30 20:11:54 -0700

    Blank window(Unicode error)

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,43 @@
 // src/main.cpp
-#include <iostream>
+#include <windows.h>
+#define UNICODE
+#define _UNICODE
 
-int main() {
-    std::cout << "Welcome to Poet's Desk!" << std::endl;
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    if (uMsg == WM_DESTROY) {
+        PostQuitMessage(0);
+        return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR, int nCmdShow) {
+    const wchar_t CLASS_NAME[] = L"PoetsDeskWindowClass";
+
+    WNDCLASS wc = { };
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = hInstance;
+    wc.lpszClassName = CLASS_NAME;
+
+    RegisterClass(&wc);
+
+    HWND hwnd = CreateWindowEx(
+        0, CLASS_NAME, L"Poet's Desk",
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT, CW_USEDEFAULT, 800, 600,
+        nullptr, nullptr, hInstance, nullptr
+    );
+
+    if (hwnd == nullptr) return 0;
+
+    ShowWindow(hwnd, nCmdShow);
+
+    MSG msg = { };
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
     return 0;
 }
